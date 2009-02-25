edc0f820d097b4e5f999279543e9c4758e25d68f
Robert P. Goldman <rpgoldman@sift.net>
2009-02-25 14:43:32 +0000

    Modified the in-package form to be compatible with Allegro's modern lisp.

--- xmlrep-helpers.lisp
+++ xmlrep-helpers.lisp
@@ -10,7 +10,7 @@
 ;;;   [2004/09/15:Robert P. Goldman] Created.
 ;;;
 ;;;---------------------------------------------------------------------------
-(in-package "XMLS")
+(in-package :xmls)
 
 (defun make-xmlrep (tag &key attribs children)
   `(,tag ,attribs ,@children))
@@ -33,49 +33,49 @@
 (defun xmlrep-find-child-tags (tag treenode)
   "Find all the children of TREENODE with TAG."
   (remove-if-not #'(lambda (child) (xmlrep-tagmatch tag child))
-		 (xmlrep-children treenode)))
+                 (xmlrep-children treenode)))
 
 (defun xmlrep-find-child-tag (tag treenode
-				  &optional (if-unfound :error))
+                                  &optional (if-unfound :error))
   "Find a single child of TREENODE with TAG.  Returns an error
 if there is more or less than one such child."
   (let ((matches (xmlrep-find-child-tags tag treenode)))
     (case (length matches)
       (0 (if (eq if-unfound :error)
-	     (error "Couldn't find child tag ~A in ~A"
-		tag treenode)
-	     if-unfound))
+             (error "Couldn't find child tag ~A in ~A"
+                tag treenode)
+             if-unfound))
       (1 (first matches))
       (otherwise (error "Child tag ~A multiply defined in ~A"
-			tag treenode)))))
+                        tag treenode)))))
 
 (defun xmlrep-attrib-value (attrib treenode
-			    &optional (if-undefined :error))
+                            &optional (if-undefined :error))
   "Find the value of ATTRIB, a string, in TREENODE.
 if there is no ATTRIB, will return the value of IF-UNDEFINED,
 which defaults to :ERROR."
   (let ((found-attrib (find attrib (xmlrep-attribs treenode)
-			    :test #'string=
-			    :key #'car)))
+                            :test #'string=
+                            :key #'car)))
     (cond (found-attrib
-	   (second found-attrib))
-	  ((eq if-undefined :error)
-	   (error "XML attribute ~S undefined in ~S"
-		  attrib treenode))
-	  (t
-	   if-undefined))))
+           (second found-attrib))
+          ((eq if-undefined :error)
+           (error "XML attribute ~S undefined in ~S"
+                  attrib treenode))
+          (t
+           if-undefined))))
 
 (defun xmlrep-boolean-attrib-value (attrib treenode
-			    &optional (if-undefined :error))
+                            &optional (if-undefined :error))
   "Find the value of ATTRIB, a string, in TREENODE.
-The value should be either \"true\" or \"false\".  The 
-function will return T or NIL, accordingly.  If there is no ATTRIB, 
+The value should be either \"true\" or \"false\".  The
+function will return T or NIL, accordingly.  If there is no ATTRIB,
 will return the value of IF-UNDEFINED, which defaults to :ERROR."
   (let ((val (xmlrep-attrib-value attrib treenode
-				  if-undefined)))
+                                  if-undefined)))
     (cond ((string-equal val "true")
-	   t)
-	  ((string-equal val "false") nil)
-	  (t (error "Not a boolean value, ~A for attribute ~A."
-		    val attrib)))))
+           t)
+          ((string-equal val "false") nil)
+          (t (error "Not a boolean value, ~A for attribute ~A."
+                    val attrib)))))
 
