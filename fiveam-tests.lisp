7fffdf0da9e9fb30644fc3778f8ca532c0578b4b
Robert P. Goldman <rpgoldman@sift.net>
2017-11-22 17:32:06 +0000

    Fix issue for allegromodern.
    Not a bug in XMLS; just a bug in the test.

--- fiveam-tests.lisp
+++ fiveam-tests.lisp
@@ -21,7 +21,7 @@
               (parse "<description><![CDATA[Link to Catalog In this sequel to 2010's surprise hit, Greg Heffley, the kid who made \"wimpy\" cool is back in an all-new family comedy based on the best-selling follow-up novel by Jeff Kinney. (Kinney's Wimpy Kid\" series has thus far sold 42 million books.) As he begins seventh grade, Greg and his older brother [...]]]></description>"))))
 
 (test simple-nodelist-translator
-  (is (equalp (nodelist->node (list "description" NIL
+  (is (equalp (nodelist->node (list "description" nil
  "Link to Catalog In this sequel to 2010's surprise hit, Greg Heffley, the kid who made \"wimpy\" cool is back in an all-new family comedy based on the best-selling follow-up novel by Jeff Kinney. (Kinney's Wimpy Kid\" series has thus far sold 42 million books.) As he begins seventh grade, Greg and his older brother [...]"))
               (parse "<description><![CDATA[Link to Catalog In this sequel to 2010's surprise hit, Greg Heffley, the kid who made \"wimpy\" cool is back in an all-new family comedy based on the best-selling follow-up novel by Jeff Kinney. (Kinney's Wimpy Kid\" series has thus far sold 42 million books.) As he begins seventh grade, Greg and his older brother [...]]]></description>"))))
 
