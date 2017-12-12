73190cbef4d25a1e5e79512de12c2f6f5edde59b
Robert P. Goldman <rpgoldman@sift.net>
2017-12-12 15:53:39 +0000

    Try to ensure that errors reach the test script.

--- octets-xml.lisp
+++ octets-xml.lisp
@@ -85,7 +85,7 @@
           for parsed = (with-open-file (octets (asdf:system-relative-pathname
                                                 :xmls/octets test-case)
                                                :element-type '(unsigned-byte 8))
-                         (xmls:parse (make-xml-stream octets)))
+                         (xmls:parse (make-xml-stream octets) nil))
           as parsed-list = (xmls:node->nodelist parsed)
           do (when (not (equal parsed-list reference))
                (setf success-p nil)
