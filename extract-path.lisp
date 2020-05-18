5e1071731c0690aa6e5fc0d7dbfd5e854fd7ab1d
Daniel Eliason <61798218+a-daniel-eliason@users.noreply.github.com>
2020-05-18 14:59:21 +0200

    EXTRACT-PATH can match multiple attribute/value pairs. Revise doc.

--- extract-path.lisp
+++ extract-path.lisp
@@ -5,41 +5,49 @@
 ;; XML extraction tool
 
 (defun extract-path ( key-list xml )
-  "Extracts data from XML parse tree.  KEY-LIST is a path for descending down named
-objects in the argument XML.  For each path element, enclosed XML subforms are
-searched for a matching name.  Finally the whole last XML subform on the path is
-normally returned; however the symbol * may be added at the end of KEY-LIST to return
-list of all objects enclosed by the last subform on the path. KEY-LIST may be dotted
-as explained below to return XML tag attributes from the last subform on the path.
+  "Extracts data from XML parse tree.  KEY-LIST is a path for descending down
+named objects in the XML parse tree.  For each KEY-LIST element, XML subforms
+are searched for a matching tag name.  Finally the whole last XML subform on the
+path is normally returned if found; however the symbol * may be added at the end
+of KEY-LIST to return list of all objects /enclosed/ by the last subform on
+KEY-LIST. Also KEY-LIST may be dotted as explained below to return XML tag
+attributes from the last subform on KEY-LIST.
 
-XML has the parse form as returned by XMLS:PARSE-TO-LIST or XMLS:NODE->NODELIST:
-        (tag-name (attributes-list) subform*)
+XML is to have the forms as returned by PARSE-TO-LIST or PARSE:
+        (tag-name (attributes-list) subform*),
+        ((tag-name . name-space) (attributes-list) subform*), or
+        #s(node :name tag-name
+                :ns name-space
+                :attrs attributes-list
+                :children subform*)
 
-The first element in KEY-LIST must match the top level form in the XML.  Subsequently
-each element in the KEY-LIST is to match the name of a subform.
+The first element in KEY-LIST must match the top level form in XML.
+Subsequently each element in the KEY-LIST is to match a subform.
 
-An element of KEY-LIST may be a string atom.  In that case the first subform with
-tag name matching the string is matched.  An element of KEY-LIST may also be a list of
-string atoms in this format:
-        (tag-name (attribute-name attribute-value))
+An element of KEY-LIST may be a string atom.  In that case the first subform
+with tag-name matching the string is matched.  An element of KEY-LIST may also
+be a list of string atoms in this format:
+        (tag-name (attribute-name attribute-value) ...)
 
-The first subform with name matching TAG-NAME and having an attribute matching
-ATTRIBUTE-NAME with value matching ATTRIBUTE-VALUE is matched.
+The first subform with name matching TAG-NAME /and/ having attributes matching
+attribute-names and attribute-values is matched.  Zero or more attribute/value
+pairs may be given.
 
-Normally the whole subform matching last element in KEY-LIST is returned.  The symbol
-* can be the last element of KEY-LIST to return list of all subforms enclosed by last
-matched form.  Attributes of last matched subform may be searched by ending KEY-LIST
-in dot notation, in which case the string after dot matches an attribute name.  The
-two element list of attribute name and value is returned.  * may be used after dot to
-return the whole attribute list.
+Normally the whole subform matching last element in KEY-LIST is returned.  The
+symbol * can be the last element of KEY-LIST to return list of all subforms
+enclosed by the last matched form.  Attributes of last matched subform may be
+searched by ending KEY-LIST in dot notation, in which case the string after dot
+matches an attribute name.  The two element list of attribute name and value is
+returned. The symbol * may be used after dot to return the whole attribute list.
 
-In the case where the search fails NIL is returned.  However it is possible that the
-search partially succeeds down the key path.  Three values are returned altogether
-and the state of the 2nd and 3rd values gives information about how much of KEY-LIST
-was matched, and at what point in XML:
+In the case where the search fails NIL is returned.  However it is possible that
+the search partially succeeds down the key path.  Three values are returned
+altogether and the 2nd and 3rd values give information about how much of
+KEY-LIST was matched, and at what point in XML:
         (values RESULT  KEY-LIST-FRAGMENT  XML-FRAGMENT)
 
-When RESULT is non-NIL, the others are NIL. When result is NIL however, the others are:
+When RESULT is non-NIL, the others are NIL. When result is NIL however, the
+others are:
         XML-FRAGMENT
           The last XML form that /did/ match in the key list.  It matches the first
           element of KEY-LIST-FRAGMENT.
@@ -52,7 +60,19 @@ When RESULT is non-NIL, the others are NIL. When result is NIL however, the othe
 In the case of complete failure, where even the very first item on KEY-LIST does not
 match the top XML form given, all three return values are NIL.  (It suffices to check
 the first two return values.)"
-  (labels ((find-test ( key xml-form )
+  (labels ((attribs-match-p ( key-attribs-list xml-attribs-list )
+             ;; search for (attr-name attr-value) pairs from KEY-ATTRIBS-LIST on
+             ;; XML-ATTRIBS-LIST.  true if all key pairs found.
+             (loop
+                :with attribs-match-var := t
+                :for attrib-key-pair  :in key-attribs-list
+                :do
+                  (setq attribs-match-var
+                        (and attribs-match-var
+                             (find attrib-key-pair xml-attribs-list :test #'equal)))
+                :finally (return attribs-match-var)))
+
+           (find-test ( key xml-form )
              ;; test whether the XML-FORM matches KEY
              (cond
                ;; just the XML tag name in key
@@ -61,10 +81,9 @@ the first two return values.)"
                      (stringp (xmlrep-tag xml-form)))
                 (string-equal key (xmlrep-tag xml-form)))
 
-               ;; key form (tag-name (attr-name attr-value))
-               (t
-                (and (find-test (car key) xml-form)
-                     (find (cadr key) (xmlrep-attribs xml-form) :test #'equal)))))
+               ;; key form (tag-name (attr-name attr-value) ...)
+               ((and (find-test (car key) xml-form)
+                     (attribs-match-p (cdr key) (xmlrep-attribs xml-form))))))
 
            (descend ( key-list xml-form )
              ;; recursive run down KEY-LIST.  If XML-FORM runs down to NIL before reaching
